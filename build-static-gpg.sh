45b120c2300d25e0f444c43f831b95164f466b01
Anand Avati <avati@gluster.com>
2011-02-04 22:08:14 -0800

    complete build system and scripts integration

--- build-static-gpg.sh
+++ build-static-gpg.sh
@@ -1,17 +1,8 @@
 #!/bin/bash -e
 
-buildspace="/tmp/build-static-gpg";
 gnupg_version="gnupg-1.4.11"
 url="ftp://ftp.gnupg.org/gcrypt/gnupg/$gnupg_version.tar.bz2"
 url="http://ftp.heanet.ie/disk1/ftp.gnupg.org/gcrypt/gnupg/$gnupg_version.tar.bz2"
-
-function enter_buildspace()
-{
-    rm -rvf $buildspace/$gnupg_version;
-    mkdir -pv $buildspace;
-    cd $buildspace;
-}
-
 
 function prepare_source()
 {
@@ -23,16 +14,16 @@ function prepare_source()
 
 function build_static_gpg()
 {
-    ./configure CFLAGS="-static";
+    ./configure --quiet CFLAGS="-static";
     make --quiet;
 }
 
 
 function main()
 {
-    (enter_buildspace; prepare_source; build_static_gpg;)
+    (prepare_source; build_static_gpg;)
 
-    cp -v $buildspace/$gnupg_version/g10/gpgv .;
+    cp -v $gnupg_version/g10/gpgv .;
 }
 
 main "$@"
